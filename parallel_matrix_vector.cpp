5256edc3c6717ac35e26945bb4add0f9a0e9c661
Chaitali Narayane <92903135+ChaitaliNarayane2104@users.noreply.github.com>
2023-05-25 03:21:44 +0530

    Update parallel_matrix_vector.cpp

--- parallel_matrix_vector.cpp
+++ parallel_matrix_vector.cpp
@@ -59,3 +59,25 @@ int main()
     
     return 0;
 }
+
+/*
+The code you provided is a C++ code that performs vector-matrix multiplication using OpenMP for parallel execution. It multiplies each row of the matrix by the vector and stores the result in the rvec vector.
+
+Here's how the code works:
+
+It prompts the user to enter the number of elements in the vector (nvec).
+It initializes the vec vector with random values between 0 and 9.
+It prompts the user to enter the number of rows in the matrix (rows).
+It initializes the mat matrix with random values between 0 and 9.
+It prints the contents of the vector and matrix.
+It initializes the rvec vector, which will store the result of the vector-matrix multiplication.
+It uses OpenMP to parallelize the computation of each row of the matrix.
+For each row, it initializes an array arr with zeros and multiplies each element of the row by the corresponding element in the vector.
+It calculates the sum of the elements in the arr array using OpenMP's reduction clause.
+It assigns the sum to the corresponding element in the rvec vector.
+Finally, it prints the result vector rvec.
+Note that there are a few issues with the code:
+
+The line int rvec[nvec]; should be int rvec[rows]; since rvec should have the same number of elements as the number of rows in the matrix.
+The array arr is declared inside the parallel for loop, which creates a new array for each iteration. To fix this, move the declaration of arr before the parallel for loop.
+*/
